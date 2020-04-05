6f18428b470db812e24ccffaa3665ec52acfc3d3
Thanyathon <61010497@kmitl.ac.th>
2020-04-05 12:16:46 +0700

    Rankking funtional

---
+++ src/helpers/index.js
@@ -0,0 +1,8 @@
+
+export const leveling = likes => {
+  if (likes >= 50) return { level: 5, color: 'purple', tile: 'Master Teacher' }
+  else if (likes >= 30) return { level: 4, color: 'red', tile: 'Expert Teacher' }
+  else if (likes >= 15) return { level: 3, color: 'blue', tile: 'Intermediate Teacher' }
+  else if (likes >= 5) return { level: 2, color: 'yellow', tile: 'Beginner Teacher' }
+  else return { level: 1, color: 'green', title: 'Novince Teacher' }
+}
--- src/pages/Ranking/Ranking.js
+++ src/pages/Ranking/Ranking.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Ranking.scss'
 
+import { leveling } from '../../helpers'
 import { profiles } from '../../img/Profile'
 
 export default () => {
+  const [teacher, setTeacher] = useState([])
+
+  useEffect(() => {
+    fetch(window.$ENDPOINT + '/all-teachers', {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(json => {
+        const { teachers, error } = json
+
+        if (teachers) {
+          const sorted = teachers.sort((a, b) => b.likes - a.likes)
+          setTeacher(sorted)
+        } else {
+          alert(error)
+        }
+      })
+  }, [])
+
   return (
     <div className='ranking-bg'>
       <div className='ranking-content'>
@@ -12,42 +32,53 @@ export default () => {
 
           <div className='not-top'>
             <header>SECOND</header>
-            <img className='profile' src={profiles[8]} alt='' />
-            <label>username</label>
-            <p>LV 3</p>
+            <img
+              className={`profile ${leveling(teacher[1]?.likes).color}`}
+              src={profiles[teacher[1]?.profile_icon]}
+              alt=''
+            />
+            <label>{teacher[1]?.name}</label>
+            <p className={`${leveling(teacher[1]?.likes).color}`}>LV {leveling(teacher[1]?.likes).level}</p>
           </div>
 
           <div className='top'>
             <header>FIRST</header>
-            <img className='profile' src={profiles[8]} alt='' />
-            <label>username</label>
-            <p>LV 5</p>
+            <img
+              className={`profile ${leveling(teacher[0]?.likes).color}`}
+              src={profiles[teacher[0]?.profile_icon]}
+              alt=''
+            />
+            <label>{teacher[0]?.name}</label>
+            <p className={`${leveling(teacher[0]?.likes).color}`}>LV {leveling(teacher[0]?.likes).level}</p>
           </div>
 
           <div className='not-top'>
             <header>THIRD</header>
-            <img className='profile' src={profiles[8]} alt='' />
-            <label>username</label>
-            <p>LV 2</p>
+            <img
+              className={`profile ${leveling(teacher[2]?.likes).color}`}
+              src={profiles[teacher[2]?.profile_icon]}
+              alt=''
+            />
+            <label>{teacher[2]?.name}</label>
+            <p className={`${leveling(teacher[2]?.likes).color}`}>LV {leveling(teacher[2]?.likes).level}</p>
           </div>
 
         </div>
         <div className='rank-list'>
-          <span>4.
-            <img className='profile' src={profiles[2]} alt='' />
-            <label>username</label>
-            <p>LV 5</p>
-          </span>
-          <span>5.
-            <img className='profile' src={profiles[2]} alt='' />
-            <label>username</label>
-            <p>LV 5</p>
-          </span>
-          <span>6.
-            <img className='profile' src={profiles[2]} alt='' />
-            <label>username</label>
-            <p>LV 5</p>
-          </span>
+
+          {teacher.map((t, index) => index > 2 ?
+            <span key={index}>{index + 1}.
+              <img
+                className={`profile ${leveling(teacher[index]?.likes).color}`}
+                src={profiles[teacher[index]?.profile_icon]}
+                alt=''
+              />
+              <label>{t.name}</label>
+              <p className={`${leveling(teacher[index]?.likes).color}`}>LV {leveling(teacher[index]?.likes).level}</p>
+            </span>
+            : null
+          )}
+
         </div>
       </div>
     </div>
